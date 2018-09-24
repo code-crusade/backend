69822cc1d5d6b38122740cf0fd692c11246da18c
GDF <gabriel.dupontf@gmail.com>
2018-09-24 18:31:28 -0400

    Fixed springfox bean missing.

--- src/main/java/com/etsmtl/codecrusade/ServiceApplication.java
+++ src/main/java/com/etsmtl/codecrusade/ServiceApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
-@EnableSwagger2WebMvc
-@Import(SpringDataRestConfiguration.class)
+@Import({ SpringDataRestConfiguration.class })
 public class ServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApplication.class, args);
