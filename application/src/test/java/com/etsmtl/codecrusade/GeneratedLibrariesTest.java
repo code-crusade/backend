4fdfc8610beea34efad4cbabb69153add5a15423
GDF <gabriel.dupontf@gmail.com>
2018-10-31 12:06:59 -0400

    Mavenized project

--- application/src/test/java/com/etsmtl/codecrusade/GeneratedLibrariesTest.java
+++ application/src/test/java/com/etsmtl/codecrusade/GeneratedLibrariesTest.java
@@ -1,14 +1,13 @@
 package com.etsmtl.codecrusade;
 
-import com.etsmtl.codecrusade.model.RFC7807Body;
 import org.junit.Test;
 
 public class GeneratedLibrariesTest {
 
-	private RFC7807Body body;
+	//private RFC7807Body body;
 
 	@Test
 	public void testClassResolves(){
-		this.body = new com.etsmtl.codecrusade.model.RFC7807Body();
+		//this.body = new com.etsmtl.codecrusade.model.RFC7807Body();
 	}
 }
