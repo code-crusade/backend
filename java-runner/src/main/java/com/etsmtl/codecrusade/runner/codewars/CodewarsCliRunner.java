5b6d39758608025e801c899afaf1ea10b1c020f3
remz <remz906@gmail.com>
2018-11-28 16:37:20 -0500

    For safer use of cli used apache wrapper  - Supports time;  - Supports single executable;

--- java-runner/src/main/java/com/etsmtl/codecrusade/runner/codewars/CodewarsCliRunner.java
+++ java-runner/src/main/java/com/etsmtl/codecrusade/runner/codewars/CodewarsCliRunner.java
@@ -2,27 +2,71 @@ package com.etsmtl.codecrusade.runner.codewars;
 
 import com.etsmtl.codecrusade.runner.Runner;
 import com.etsmtl.codecrusade.runner.RunnerResult;
+import org.apache.commons.exec.*;
+import org.apache.commons.lang3.time.StopWatch;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CodewarsCliRunner implements Runner {
 
     @Override
     public RunnerResult run(Language lang, String code, String test) {
-        return this.run(CodewarsLanguage.fromRunnerLanguage(lang), 0, code, test);
+        return this.runSafe(CodewarsLanguage.fromRunnerLanguage(lang), ExecuteWatchdog.INFINITE_TIMEOUT, code, test);
     }
 
     @Override
     public RunnerResult run(Language lang, long timeout, String code, String test) {
-        return this.run(CodewarsLanguage.fromRunnerLanguage(lang), timeout, code, test);
+        return this.runSafe(CodewarsLanguage.fromRunnerLanguage(lang), timeout, code, test);
     }
 
-    // TODO: Manage timeout.
-    // TODO: Manage multiple tests?
+    private CodewarsRunnerResult runSafe(CodewarsLanguage lang, long timeout, String code, String test) {
+        CodewarsRunnerResult result;
+
+        CommandLine cmd = new CommandLine("docker")
+                .addArgument("run")
+                .addArgument(lang.getContainerName())
+                .addArgument("run")
+                .addArgument("-l")
+                .addArgument(lang.getRunnerName())
+                .addArgument("-c")
+                .addArgument(String.format("\"%s\"", code.replaceAll("\"", "\\\"")), false)
+                .addArgument("-t")
+                .addArgument(lang.getTestFormat())
+                .addArgument(String.format("\"%s\"", test.replaceAll("\"", "\\\"")), false);
+
+        DefaultExecutor executor = new DefaultExecutor();
+        executor.setWatchdog(new ExecuteWatchdog(timeout));
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        executor.setStreamHandler(new PumpStreamHandler(out));
+
+        StopWatch watch = StopWatch.createStarted();
+
+        try {
+            if (executor.execute(cmd) == 0) {
+                watch.stop();
+                result = new CodewarsRunnerResult(Arrays.asList(out.toString().split("\n")), watch.getTime());
+
+            } else {
+                watch.stop();
+                result = new CodewarsRunnerResult(out.toString(), watch.getTime(), RunnerResult.Status.ERROR);
+            }
+        } catch (IOException e) {
+            watch.stop();
+            result = new CodewarsRunnerResult(e.toString(), watch.getTime(), RunnerResult.Status.ERROR);
+        }
+
+        return result;
+    }
+
+    @Deprecated
     private CodewarsRunnerResult run(CodewarsLanguage lang, long timeout, String code, String test) {
         Runtime rt = Runtime.getRuntime();
 
